86b1eba1ab72e60fd80c79a4f2943475a178932a
indywip <indira.sowy@gmail.com>
2022-02-11 13:28:09 -0800

    Made tests for MyClass

--- src/test/model/MyClassTest.java
+++ src/test/model/MyClassTest.java
@@ -1,5 +1,21 @@
 package model;
 
-public class MyClassTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyClassTest {
+    private MyClass testClasses;
+
+    @BeforeEach
+    void runBefore() {
+    testClasses = new MyClass();
+    }
+
+    @Test
+    void testConstructor() {
+        testClasses.setClass("CPSC110");
+        assertEquals("CPSC110", testClasses.retrieveClass());
+    }
 
 }
